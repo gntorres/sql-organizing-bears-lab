5b4f3c4589eccd38ebf24ceadd141329987f0b6f
gntorres <gntorres421@gmail.com>
2021-02-18 22:26:09 -0600

    commit tree e691f1dbc753d93b516ddb15ab832cefcc6d0aed

---
+++ lib/create.sql
@@ -0,0 +1,10 @@
+CREATE TABLE bears (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(20),
+    age INTEGER,
+    sex VARCHAR(1),
+    color VARCHAR(15),
+    temperament VARCHAR(20),
+    alive INTEGER
+    
+);
---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', '20', 'M', 'Brown', 'Nice', '1');
+INSERT INTO bears VALUES (2, 'Rowdy', '20', 'M', 'Brown', 'Nice', '1');
+INSERT INTO bears VALUES (3, 'Tabitha', '20', 'F', 'Brown', 'Nice', '1');
+INSERT INTO bears VALUES (4, 'Sergeant Brown', '20', 'M', 'Brown', 'Nice', '1');
+INSERT INTO bears VALUES (5, 'Melissa', '20', 'F', 'Brown', 'Nice', '1');
+INSERT INTO bears VALUES (6, 'Grinch', '20', 'M', 'Brown', 'Nice', '1');
+INSERT INTO bears VALUES (7, 'Wendy', '20', 'F', 'Brown', 'Nice', '1');
+INSERT INTO bears(id, age, sex, color, temperament, alive) VALUES (8, '20', 'M', 'Brown', 'Nice', '1');
